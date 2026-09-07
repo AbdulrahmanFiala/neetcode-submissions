b52f272a0e5168542b394a454c95d1215a687d3a
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2026-09-07 05:00:45 +0300

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !isAlpha(s.charAt(l))) {
+                l++;
+            }
+            while (l < r && !isAlpha(s.charAt(r))) {
+                r--;
+            }
+
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9');
+    }
+}
